a6ff386b0dca437549eeea70725be9ffef121296
VishamRohilla <96700866+VishamRohilla@users.noreply.github.com>
2024-02-04 20:18:09 +0530

    Update Dataform.js

--- src/components/Dataform.js
+++ src/components/Dataform.js
@@ -30,7 +30,7 @@ export default function Dataform() {
     setLoading(true);
     setFormSubmitted(true);
     axios
-      .post("https://fitness-ai-omega.vercel.app/", {
+      .post("https://fitness-api-eosin.vercel.app/", {
         height,
         weight,
         primarygoal,
